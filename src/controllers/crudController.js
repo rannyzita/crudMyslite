6d517cc677fe10ca232ba73a7a360545c0992b97
rannyzita <fs.vitoria.soares@gmail.com>
2024-12-11 21:18:53 -0300

    aprimorando a segurança dos dados

--- src/controllers/crudController.js
+++ src/controllers/crudController.js
@@ -8,6 +8,25 @@ module.exports = {
         return res.json(Alunos);
     },
 
+    async GetId (req, res) {
+        try {
+            // desestruturação
+            // td q tiver no dado, vou querer somente o id
+            const {id} = req.params;
+            const existe = await connection("Alunos").where("id", id).first();
+            let Aluno = connection;
+
+            if (!existe) {
+                return res.status(404).send("Id não encontrado");
+            } else {
+                Aluno = await connection("Alunos").select("*").where("id", id);
+                return res.json(Aluno);
+            }
+        } catch (error) {
+            return res.status(500).send(error);
+        }
+    },
+
     async Post (req, res) {
         const {Nome, idade, NumChamada} = req.body;
 
@@ -19,5 +38,39 @@ module.exports = {
         });
 
         return res.json({id});
+    },
+
+    async Update (req, res) {
+        try {
+            const {id} = req.params;
+            const {Nome, idade, NumChamada} = req.body;
+            const existe = await connection("Alunos").where("id", id).first();
+
+            if(!existe) {
+                return res.status(404).send("Id não encontrado");
+            } else {
+                // vai fazer a inserção do dado
+                await connection("Alunos").update({
+                Nome: Nome,
+                idade: idade,
+                NumChamada: NumChamada,
+            }).where("id", id);
+                return res.status(200).send("Deu certo a alteração do registro.");
+            }
+        } catch (error) {
+            return res.status(500).send(error);
+        } 
+    },
+
+    async Delete (req, res) {
+        try {
+            const {id} = req.params;
+
+            // vai fazer a inserção do dado
+            await connection("Alunos").delete().where("id",id);
+            return res.status(200).send("Deu certo a remoção do registro.");
+        } catch (error) {
+            return res.status(500).send(error);
+        } 
     },
 }
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,11 @@ const crudController = require("./controllers/crudController.js");
 
 const routes = express.Router();
 
+routes.get("/AllAlunos", crudController.GetAll);
+routes.get("/GetId/:id", crudController.GetId);
 routes.post("/insertAluno", crudController.Post);
+routes.put("/UpdateAluno/:id", crudController.Update);
+routes.delete("/DeleteAluno/:id", crudController.Delete);
 
 module.exports = routes;    
 
